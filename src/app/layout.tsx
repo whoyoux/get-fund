d90fcef685cea686857fd8ea949d3e19e0b3f8bc
whoyoux <48690153+whoyoux@users.noreply.github.com>
2023-12-25 19:46:05 +0100

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import { Inter as FontSans } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
 import { siteConfig } from "@/config/site";
-import Nav from "@/components/nav";
+import Header from "@/components/header";
 
 export const fontSans = FontSans({
     subsets: ["latin"],
@@ -46,7 +46,7 @@ export default function RootLayout({
                     enableSystem
                     disableTransitionOnChange
                 >
-                    <Nav />
+                    <Header />
                     <main className="px-4 md:px-8 xl:px-12">{children}</main>
                 </ThemeProvider>
             </body>
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,17 +2,14 @@ import Link from "next/link";
 import { Button } from "./ui/button";
 import { ThemeSwitcher } from "./theme-switcher";
 
-const Nav = () => {
+const Header = async () => {
     return (
         <nav className="w-full flex items-center justify-between px-4 md:px-8 xl:px-12 py-4 md:py-8 bg-background border-b shadow">
             <Link href="/">
                 <h1 className="font-bold text-xl md:text-2xl">Get Fund</h1>
             </Link>
             <div className="flex items-center gap-2">
-                <Link href="/sign-up">
-                    <Button>Create your fund</Button>
-                </Link>
-                <Link href="/sign-in">
+                <Link href="/">
                     <Button>Sign in</Button>
                 </Link>
                 <ThemeSwitcher />
@@ -21,4 +18,4 @@ const Nav = () => {
     );
 };
 
-export default Nav;
+export default Header;
